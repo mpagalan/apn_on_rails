38484e8ca754976d3b55fcbcfe9a80e7bbda0ba5
Marjun Pagalan <mpagalan@gmail.com>
2013-05-16 17:05:25 +0800

    fix migration templates as we dont care for backwards compatibility

--- generators/templates/apn_migrations/001_create_apn_devices.rb
+++ generators/templates/apn_migrations/001_create_apn_devices.rb
@@ -1,10 +1,13 @@
 class CreateApnDevices < ActiveRecord::Migration # :nodoc:
   def self.up
     create_table :apn_devices do |t|
-      t.text :token, :size => 71, :null => false
+      t.text     :token, :size => 100, :null => false
+      t.integer  :app_id
+      t.datetime :last_registered_at
 
       t.timestamps
     end
+    add_index :apn_devices, :token
   end
 
   def self.down
